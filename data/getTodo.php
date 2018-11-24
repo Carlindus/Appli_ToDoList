5101167fbb167fb7e807926b152c2e177db163a0
carlindus <carlindusdesign@gmail.com>
2018-11-24 10:40:36 +0100

    Suppress Data files

--- data/getTodo.php
+++
@@ -1,5 +0,0 @@
-<?php
- header("Access-Control-Allow-Origin: *");
-
-$homepage = file_get_contents('toDoList.json');
-echo $homepage;
--- data/saveTodo.php
+++
@@ -1,7 +0,0 @@
-<?php
- // header("Access-Control-Allow-Origin: *");
-
- $json_data = json_encode($_POST);
-file_put_contents('toDoList.json', $json_data);
-
-?>
